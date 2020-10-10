a0499fd604c59a46dc21205790d4c982591edaa0
RafaS92 <manrvaldez..92@gmail.com>
2020-10-10 13:15:21 -0500

    schema define

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ mongoose
   .connect("mongodb://localhost/playground")
   .then(() => console.log("coneccted to mongodb"))
   .catch((err) => console.error("cold not coneccte", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
